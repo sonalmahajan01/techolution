bdc18405cc6a91eab788a197dc1a8f0884b30ac8
sonalmahajan01 <sonal.mahajan@vocanic.com>
2018-01-07 13:23:46 +0800

    Create consecutivesum.java

---
+++ consecutivesum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Solution{
+
+     public static void main(String []args){
+        Scanner in = new Scanner(System.in);
+        
+        long num = in.nextLong();
+        int noofWays = consecutive(num);
+        System.out.println(noofWays);
+ 
+     }
+     
+    static int consecutive(long num) {
+          
+        int max = (int)num/2;
+        int output = 0;
+        
+        for(int i = 1; i <= max; i++) {
+            int sum = i;
+            for(int j = i+1; sum < num; j++) {
+                sum +=j;
+                if(sum == num) {
+                    output++;
+                }
+            }
+        }
+        return output;
+    }
+}
